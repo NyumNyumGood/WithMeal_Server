c2a0460447b7a363642eec3896b84fd52486e0bd
Gyunny <wjdrbs966@naver.com>
2021-11-26 01:41:02 +0900

    Feat: 토큰 재발급 API 완료

--- src/main/java/com/withmeal/controller/TokenController.java
+++ src/main/java/com/withmeal/controller/TokenController.java
@@ -1,5 +1,8 @@
 package com.withmeal.controller;
 
+import com.withmeal.aop.Auth;
+import com.withmeal.aop.AuthContext;
+import com.withmeal.aop.ReAuth;
 import com.withmeal.dto.response.ApiResponse;
 import com.withmeal.dto.response.token.TokenResponseDTO;
 import com.withmeal.service.JwtService;
@@ -7,28 +10,32 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 /**
  * created by Gyunny 2021/11/09
  */
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @RestController
-public class TestController {
+public class TokenController {
 
     private final JwtService jwtService;
 
-    @GetMapping
-    public String ping() {
-        return "pong!!";
-    }
-
     @ApiOperation("JWT 토큰 발급 임시 API")
-    @GetMapping("/api/v1/token")
+    @GetMapping("/token")
     public ApiResponse<TokenResponseDTO> getAccessToken() {
         return ApiResponse.success(HttpStatus.OK, jwtService.createTokenResponse(1L));
+    }
+
+    @ApiOperation("JWT 재발급")
+    @ReAuth
+    @PostMapping("/token/refresh")
+    public ApiResponse<TokenResponseDTO> reIssueToken() {
+        Long userId = AuthContext.getCurrentUserId();
+        return ApiResponse.success(HttpStatus.OK, jwtService.createTokenResponse(userId));
     }
 
 }
